9ee4737d14b8747db9dac021d134021a6d937636
25007839d <25007839d@gmail.com>
2024-11-16 16:36:43 +0530

    setup installed

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import find_packages , setup
 from typing import List
 
+def get_requirments()->list[str]:
+    requirments_list = list[str] = []
+
+    return requirments_list
+
 setup(
     name='sensore',
     version="0.0.1",
     author="dushyant",
     author_email="25007839d@gmail.com",
     packages=find_packages(),
-    install_requires = ["pymongo"]
+    install_requires = get_requirments()#["pymongo"]
 )
